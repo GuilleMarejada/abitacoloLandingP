cec908064610a6bd2d9b9d735cdd43973f2ef44d
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-10-07 18:07:57 +0100

    cambios en infromacion de miembros

--- src/components/home/CarruselReact.jsx
+++ src/components/home/CarruselReact.jsx
@@ -20,18 +20,6 @@ const team = [
     description:
       "Construir mundos desde cero es mi pasión. Si los convierto en proyectos tangibles, entiendo que mi papel en el mundo ya lleva un camino y un propósito significativo. ",
     linkedin: "https://www.linkedin.com/in/juliette-mallen-a001a2125/",
-    semblanza1:
-      "Soy Cathy, arquitecta con especialización en comunicación arquitectónica y experiencia en la dirección creativa de eventos culturales. He combinado mi práctica arquitectónica con la exploración del impacto social y cultural del espacio urbano. ",
-    semblanza2:
-      "Actualmente, soy Fundadora de la startup ABITACOLO, donde sigo impulsando proyectos innovadores en la rehabilitación de mobiliario y espacios habitacionales.",
-    semblanza3:
-      "Recientemente, completé un Máster en Comunicación Arquitectónica en la Universidad Politécnica de Madrid, con la tesis El miedo urbano bajo la luz, y continúo mi investigación en el departamento doctoral.",
-    semblanza4:
-      "Fui Fundadora y Directora Creativa del Festival Kmikc, liderando tres ediciones centradas en la relación entre arquitectura y cultura urbana; Arquitectura & Diseño, Imagen Urbana & Concepto, y Ciudad Emergente. Mi trabajo ha sido reconocido con becas por diseño escenográfico y por estudios de posgrado, destacando mi versatilidad en proyectos que integran lo arquitectónico con lo creativo.",
-    semblanza5:
-      "Con un fuerte compromiso social, he participado en proyectos disruptivos relacionados con derivas y psicogeografías. Además, fui miembro honorario del Consejo Consultivo del IMPLAN en México y lideré proyectos significativos en la Dirección General de Obras Públicas Municipales, como la Rehabilitación del Parque Ecológico y la Recuperación del Parque Metropolitano.",
-    semblanza6:
-      "Mi pasión es transformar ideas en realidades que mejoren nuestras condiciones sociales y humanas, combinando idealismo y pragmatismo en cada proyecto.",
   },
   {
     person: "Guillermo Perales",
@@ -40,12 +28,6 @@ const team = [
     description:
       "Además de mi carácter, el desempeño profesional y mis capacidades organizativas, me convierten en un arquitecto todo terreno. Ahora soy pilar en la startup de diseño regenerativo de mobiliario. Convertimos la basura en tesoros únicos.",
     linkedin: "https://www.linkedin.com/in/gperalesc/",
-    semblanza1:
-      "¡Hola! Soy Guille, madrileño y arquitecto habilitado por la Escuela Técnica Superior de Arquitectura de Madrid (ETSAM), con estudios complementarios en el University College de Dublín (Irlanda). También me he formado en diseño gráfico en el Istituto Marangoni de Milán (Italia) y cuento con el Máster en Comunicación Arquitectónica de la Universidad Politécnica de Madrid (UPM). ",
-    semblanza2:
-      "A lo largo de mi carrera profesional, he participado y ganado premios en diversos proyectos de urbanismo, paisajismo, arquitectura, diseño gráfico, exposiciones…. Pero mi pasión siempre han sido los muebles y el diseño de interiores. Por eso, Abitacolo es un proyecto muy personal en el que pongo en práctica todo lo que sé, siempre a la búsqueda  soluciones más sostenibles y adecuadas para cada hogar. ",
-    semblanza3:
-      "Si quieres saber más sobre mí, solo pide una reunión: ¡estaré encantado de conocerte!  Y podemos hablar en español, inglés o portugués ;)",
   },
   {
     person: "Manuel Ben",
@@ -54,14 +36,6 @@ const team = [
     description:
       "Diseñador de Productos con más de 3 años de experiencia en investigación de usuarios y desarrollo de soluciones que impulsan el crecimiento empresarial, complementado con 8 años en producción audiovisual y gestión de proyectos.",
     linkedin: "https://www.linkedin.com/in/benmanuel/",
-    semblanza1:
-      "Soy Diseñador de Productos Designer con más de 3 años de experiencia en el ecosistema digital, especializado en la investigación de usuarios, centrándome en datos cualitativos y cuantitativos para así desarrollar el mejor plan de acción. He demostrado una sólida capacidad para desarrollar soluciones centradas en el usuario que se alinean con los objetivos empresariales y generan ingresos. Mi enfoque está en crear diseños que resuelvan problemas reales y mejoren la experiencia del usuario.",
-    semblanza2:
-      "Dispongo de experiencia trabajando en proyectos de ecommerce, en el sector healthcare y anuncios clasificados. ",
-    semblanza3:
-      "Adicionalmente, cuento con más de 8 años de experiencia como Productor Audiovisual. En esta etapa, coordiné equipos de manera efectiva y desempeñé el rol de secretario de producción, lo que me permitió perfeccionar mis habilidades en gestión de proyectos,  comunicación y liderazgo.",
-    semblanza4:
-      "Creo firmemente que mi combinación de habilidades en diseño digital y producción audiovisual, me convierte en un perfil ideal para una posición donde aporte perspectivas únicas y valiosas, mucho más si me encuentro en un proyecto donde mis aportaciones sean recibidas con el entusiasmo que merecen. Abitacolo es uno de ellos.",
   },
   {
     person: "Alba Caldito G.",
@@ -71,20 +45,16 @@ const team = [
     description:
       "Hablar es mi pasión y puedo hacerlo desde cualquier canal, lo que se podría definir como “ser una comunicadora 360”. Casi sin querer, le puse voz a Abitacolo.",
     linkedin: "https://www.linkedin.com/in/albacalditogutierrez/",
-    semblanza1:
-      "Hola, soy Alba. Lo que más me gusta en el mundo es cocinar y escribir, porque ambas son la mejor expresión de mi creatividad. Si te preguntas por qué estoy aquí, es porque sé cómo encontrar el equilibrio perfecto, ya sea en la cocina o al elegir las palabras. Nunca me paso de sal, ni en los platos ni en las frases.",
   },
 
   {
     person: "Maria Para R.",
-    position: "Marketing",
+    position: "Creative Project Manager",
 
     linkImage: MPR,
     description:
       "Pienso, organizo y ejecuto ideas de todo tipo. Desde campañas de publicidad a nivel nacional a eventos en locales del barrio. Y eso hago con ABITACOLO, sacar un proyecto con espíritu de barrio a nivel intergaláctico.",
     linkedin: "https://www.linkedin.com/in/mariapararodriguez/",
-    semblanza1:
-      "¡Hola!! Soy María y me dedico a la gestión de proyectos creativos. Disfruto ordenar y reorganizar espacios, algo que aplico en mi trabajo. Cada proyecto es una oportunidad para crear algo único, estructurando ideas y entornos. Me encanta transformar el caos en orden y hacer que todo fluya. ",
   },
   {
     person: "Cristina Allen Casal",
@@ -94,8 +64,6 @@ const team = [
     description:
       "Creo marcas, resuelvo problemas y me especializo en el marketing en entornos innovadores. En resumen, me encargo de que conozcas Abitacolo y te enamores.",
     linkedin: "https://www.linkedin.com/in/cristina-allende-casal/",
-    semblanza1:
-      "¡Hola!! Soy María y me dedico a la gestión de proyectos creativos. Disfruto ordenar y reorganizar espacios, algo que aplico en mi trabajo. Cada proyecto es una oportunidad para crear algo único, estructurando ideas y entornos. Me encanta transformar el caos en orden y hacer que todo fluya. ",
   },
   {
     person: "Xavier Longo",
@@ -104,12 +72,6 @@ const team = [
     description:
       "Soy Xavi, desarrollador Full Stack con exp en proyectos innovadores.Mi habilidad más contundente sin duda es aportar soluciones creativas y eficientes en el desarrollo web.",
     linkedin: "www.linkedin.com/in/xavierlongo",
-    semblanza1:
-      "Soy Xavi, un apasionado desarrollador Full Stack con experiencia en proyectos innovadores y dominio de tecnologías como React, Astro , Python , MySQL  y Flask. Mi habilidad más contundente sin duda es aportar soluciones creativas y eficientes en el desarrollo web.",
-    semblanza2:
-      "Ahora mismo soy responsable del desarrollo de las plataformas digitales para Abitacolo, desde las páginas web hasta las aplicaciones. Éste trabajo me ha llevado a encontrar soluciones no planteadas en un entorno innovador y en sintonía con otras startup como Uelz, podría decirse que soy un detective del desarrollo web. Mi experiencia incluye la creación de backend, API y bases de datos, lo que me permite manejar el ciclo completo de desarrollo.",
-    semblanza3:
-      "Además, tengo un trasfondo en gestión empresarial como gerente en Talleres J Longo, donde he implementado sistemas de control administrativo, experiencia que me otorga habilidades en la gestión de entornos laborales y la resolución de problemas en el día a día. Mi formación en 4Geeks Academy y mi ciclo formativo en Desarrollo de Aplicaciones Web han sido clave en mi crecimiento profesional. Estoy motivado para seguir innovando y mejorando la experiencia del usuario en cada proyecto, y con Abitacolo a sostener el entorno vivo de un contenido digital que no detendrá su crecimiento.",
   },
   {
     person: "Guillermo Correa",
@@ -118,14 +80,6 @@ const team = [
     description:
       "Soy escalador y programador, mi selección de deporte define parte de lo que se ve en mi trabajo. Llegar a la cima, disfrutar el proceso, y ver el paisaje reservado para quienes nos aventuramos.",
     linkedin: "https://www.linkedin.com/in/guillermocg8/",
-    semblanza1:
-      "Soy Desarrollador Full Stack con una experiencia liderando y colaborando en equipos multifuncionales. Mi enfoque está en crear soluciones efectivas tanto en el frontend como en el backend, con un dominio en tecnologías como JavaScript, Python, React , Astro, TailwindCSS, SQLAlchemy y Flask. He trabajado en el desarrollo de plataformas Ecommerce, gestionando y coordinando equipos para asegurar la entrega de proyectos y MVPs.",
-    semblanza2:
-      "Recientemente, participé en el desarrollo de la plataforma Ecommerce Elévate, donde trabajé en la integración completa del frontend con Astro, React y Zustand, y del backend utilizando Python y SQLAlchemy. Además, en mi rol en Abitacolo, fui responsable de la coordinación del equipo de desarrollo y la gestión de proyectos, asegurando que los plazos y objetivos se cumplieran, y resolviendo problemas a lo largo del proceso de desarrollo.",
-    semblanza3:
-      "Mi formación incluye un título de Desarrollador Web Full Stack en 4Geeks Academy, complementado con estudios técnicos en electrónica y automatización. Con habilidades en Git, Docker y un fuerte enfoque en la optimización de código, estoy constantemente buscando mejorar la eficiencia de los sistemas y proyectos en los que trabajo.",
-    semblanza4:
-      "Me motiva enfrentar nuevos retos que me hagan seguir aprendiendo, resolver problemas complejos y seguir creciendo como profesional en el mundo del desarrollo web.",
   },
 ];
 
