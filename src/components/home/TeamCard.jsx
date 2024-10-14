79c24347147108f8861ca5a8fd304cf46fe05576
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-10-14 10:40:01 +0100

    ajustes de responsive, tamaño de textos y imagen dropdownClaves

--- src/components/home/TeamCard.jsx
+++ src/components/home/TeamCard.jsx
@@ -14,7 +14,7 @@ const TeamCard = ({ person, position, linkImage, description, linkedin }) => (
         height="320"
       />
     </a>
-    <div className="grid gap-1 p-3 pb-0">
+    <div className="grid gap-1 p-3 pb-0 my-auto">
       <h2 className="text-black/90 text-2xl font-semibold leading-7">
         {person}
       </h2>
@@ -27,7 +27,7 @@ const TeamCard = ({ person, position, linkImage, description, linkedin }) => (
         </p>
       </div>
     </div>
-    <div className="flex items-center gap-4 mt-auto p-3 pt-1">
+    <div className="flex items-center gap-4 p-3">
       {linkedin && (
         <a href={linkedin} target="_blank" rel="noopener noreferrer">
           <img
